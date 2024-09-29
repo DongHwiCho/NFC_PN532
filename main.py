435c27624d44532e8e5d66af7ab0a6b5c914c21b
DongHwiCho <junejoe97@inha.edu>
2024-09-30 02:59:01 +0900

    fix: SPI modulation

--- main.py
+++ main.py
@@ -2,28 +2,21 @@ import time
 import binascii
 
 # Import pn532pi module
-from pn532pi.nfc import pn532
-from pn532pi.interfaces import pn532hsu
+from pn532pi import pn532
+from pn532pi import Pn532Spi
 
-# Generate and Setting PN532 NFC Module and SPI PORT
-PN532_HSU = pn532hsu.Pn532Hsu(3)
-nfc = pn532.Pn532(PN532_HSU)
+PN532_SPI = Pn532Spi(0)
+nfc = pn532.Pn532(PN532_SPI)
+
 
 # setup Method
 def setup():
-  print("NTAG21x R/W")
-  print("-------Looking for pn532--------")
-
   nfc.begin()
 
   versiondata = nfc.getFirmwareVersion()
   if not versiondata:
     print("Didn't find PN53x board")
     raise RuntimeError("Didn't find PN53x board")  # halt
-
-  # Got ok data, print it out!
-  print("Found chip PN5 {:#x} Firmware ver. {:d}.{:d}".format((versiondata >> 24) & 0xFF, (versiondata >> 16) & 0xFF,
-                                                              (versiondata >> 8) & 0xFF))
 
   # Set the max number of retry attempts to read from a card
   # This prevents us from waiting forever for a card, which is
@@ -32,73 +25,54 @@ def setup():
   nfc.SAMConfig()
 
 
-# Read Felica Method
-def find_felica():
+'''
+input : timeout
+process : Felica 카드 인식을 기다리다가 카드를 읽으면 해당 카드의 uid를 반환한다. 인식되지 않으면 None을 반환한다.
+output : string (카드의 uid)
+'''
+def find_felica(timeout):
     systemCode = 0xFFFF # Setting System Code
     requestCode = 0x01  # System Code request
 
-    # Wait for an FeliCa type cards.
-    # When one is found, some basic information such as IDm, PMm, and System Code are retrieved.
-    print("Waiting for an FeliCa card...  ")
-    # Read Felica NFC Card
-    ret, idm, pwm, systemCodeResponse = nfc.felica_Polling(systemCode, requestCode, timeout=1000)
+    ret, idm, _, _ = nfc.felica_Polling(systemCode, requestCode, timeout=timeout)
 
-    # Timeout
-    if ret != 1:
-        print("Could not find a Felica card")
-        time.sleep(0.5)
-        return
-
-    # Read Success
-    print("Found a Felica card!")
-    print(f"UID : {binascii.hexlify(idm)}")
-    time.sleep(3)
+    if ret != 1: # Timeout
+        return None
+    else: # Read Success
+        return binascii.hexlify(idm)
 
 
-# Read ISO14443A Method
-def find_iso14443a():
-    print("Waiting for an ISO14443A card...  ")
+'''
+input : timeout
+process : ISO14443A 카드 인식을 기다리다가 카드를 읽으면 해당 카드의 uid를 반환한다. 인식되지 않으면 None을 반환한다.
+output : string (카드의 uid)
+'''
+def find_iso14443a(timeout):
     # Read ISO14443A Card
-    success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS, timeout=1000)
+    success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS, timeout=timeout)
 
-    # Read Success
-    if success:
-        print("Found a ISO14443A card!")
-        print(f"UID : {binascii.hexlify(uid)}")
-        time.sleep(3)
-    else :
-        # Timeout
-        print("Could not find a ISO14443A card")
-        time.sleep(0.5)
+    if success: # Read Success
+        return binascii.hexlify(uid)
+    else : # Timeout
+        return None
 
 
-# Read ISO15693 Method but didn't work...
-def find_other():
-    print("Waiting for an Other card...  ")
-    # Test for ISO15693 Card but can not read.....
-    success, uid = nfc.readPassiveTargetID(pn532.PN532_FELICA_424KBPS, timeout=1000)
-
-    # Read Success
-    if success:
-        print("Found a Other card!")
-        print(f"UID : {binascii.hexlify(uid)}")
-        time.sleep(3)
-    else:
-        # Timeour
-        print("Could not find a Other card")
-        time.sleep(0.5)
-
-
-# Infinite loop
-def loop():
-    # Check correct type of NFC
-    find_felica()
-    find_iso14443a()
-    find_other()
-
-
-# Main
-if __name__ == '__main__':
+'''
+input : timeout=1000
+process : 카드 인식을 기다리다가 카드를 읽으면 해당 카드의 uid를 반환한다. 인식되지 않으면 None을 반환한다.
+output : string (카드의 uid)
+'''
+def detect(timeout=1000):
+    uid = None
     setup()
+    uid = find_felica(timeout)
+    if uid is not None:
+        return uid
+    else:
+        return find_iso14443a(timeout)
+
+
+# test
+if __name__ == '__main__':
     while True:
-        loop()
+        print(detect())
